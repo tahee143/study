0194b830322618ba37ea66ba6e6001dea6ba7102
tahee143 <devtaehee@naver.com>
2024-07-24 15:53:48 +0900

    스프링 빈 메타정보 - BeanDefinition(xml 테스트 추가)

--- 스프링 핵심 원리/src/test/java/hello/core/beandefinition/BeanDefinitionTest.java
+++ 스프링 핵심 원리/src/test/java/hello/core/beandefinition/BeanDefinitionTest.java
@@ -5,10 +5,15 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
 
 public class BeanDefinitionTest {
 
-    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+    // 자바기반
+//    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    // xml 기반
+    GenericXmlApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
 
     @Test
     @DisplayName("빈 설정 메타정보 확인")
