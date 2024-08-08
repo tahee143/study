e4a495361751e57b63ceee92c161540ab3f6df34
tahee143 <devtaehee@naver.com>
2024-08-09 02:07:43 +0900

    @GetMapping 활용

---
+++ spring boot rest api/quickstart/src/main/java/hello/quickstart/controller/QuickController.java
@@ -0,0 +1,38 @@
+package hello.quickstart.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class QuickController {
+
+    @GetMapping("/dummy")
+    public String dummy() {
+        log.info("dummy");
+        return "{}";
+    }
+
+    @GetMapping("/dummy2")
+    public String dummy2() {
+        log.info("dummy2");
+        return "dummy2";
+    }
+
+    @GetMapping("/member")
+    public String getMember(@RequestParam("empNo") String empNo
+        , @RequestParam("year") int year){
+        log.info("empNo : {}", empNo);
+        log.info("year: {}", year);
+        return "ok";
+    }
+
+    @GetMapping("/company/{id}")
+    public String getCompany(@PathVariable("id") String id){
+        log.info("id : {}", id);
+        return "ok";
+    }
+}
