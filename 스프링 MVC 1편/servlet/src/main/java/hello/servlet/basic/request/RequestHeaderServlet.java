a249ec5baa7e023be05e91f565655810ac1ea3e7
tahee143 <devtaehee@naver.com>
2024-08-09 14:14:46 +0900

    HttpServletRequest 기본 사용법

--- 스프링 MVC 1편/servlet/src/main/java/hello/servlet/basic/request/RequestHeaderServlet.java
+++ 스프링 MVC 1편/servlet/src/main/java/hello/servlet/basic/request/RequestHeaderServlet.java
@@ -2,6 +2,7 @@ package hello.servlet.basic.request;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -16,7 +17,8 @@ public class RequestHeaderServlet extends HttpServlet {
 
         printStartLine(request); // start line 정보
         printHeaders(request); // Header 모든 정보
-
+        printHeaderUtils(request); // Header 편리한 조회
+        printEtc(request); // 기타정보
     }
 
     // start line 정보
@@ -40,6 +42,7 @@ public class RequestHeaderServlet extends HttpServlet {
         System.out.println("--- Headers - start ---");
 
         /*
+        // 예전 방식
          Enumeration<String> headerNames = request.getHeaderNames(); // 모든 header 정보 꺼냄
 
          while (headerNames.hasMoreElements()) {
@@ -54,4 +57,52 @@ public class RequestHeaderServlet extends HttpServlet {
         System.out.println();
     }
 
+    // Header 편리한 조회
+    private void printHeaderUtils(HttpServletRequest request) {
+        System.out.println("--- Header 편의 조회 start ---");
+        System.out.println("[Host 편의 조회]");
+        System.out.println("request.getServerName() = " + request.getServerName()); //Host 헤더
+        System.out.println("request.getServerPort() = " +
+        request.getServerPort()); //Host 헤더
+        System.out.println();
+
+        System.out.println("[Accept-Language 편의 조회]");
+        request.getLocales().asIterator()
+                .forEachRemaining(locale -> System.out.println("locale = " + locale));
+        System.out.println("request.getLocale() = " + request.getLocale());
+        System.out.println();
+
+        System.out.println("[cookie 편의 조회]");
+        if (request.getCookies() != null) {
+            for (Cookie cookie : request.getCookies()) {
+                System.out.println(cookie.getName() + ": " + cookie.getValue());
+            }
+        }
+        System.out.println();
+
+        System.out.println("[Content 편의 조회]");
+        System.out.println("request.getContentType() = " + request.getContentType());
+        System.out.println("request.getContentLength() = " + request.getContentLength());
+        System.out.println("request.getCharacterEncoding() = " + request.getCharacterEncoding());
+
+        System.out.println("--- Header 편의 조회 end ---");
+        System.out.println();
+    }
+
+    // 기타 정보
+    private void printEtc(HttpServletRequest request) {
+        System.out.println("--- 기타 조회 start ---");
+        System.out.println("[Remote 정보]");
+        System.out.println("request.getRemoteHost() = " + request.getRemoteHost());
+        System.out.println("request.getRemoteAddr() = " + request.getRemoteAddr()); //
+        System.out.println("request.getRemotePort() = " + request.getRemotePort()); //
+        System.out.println();
+
+        System.out.println("[Local 정보]");
+        System.out.println("request.getLocalName() = " + request.getLocalName());
+        System.out.println("request.getLocalAddr() = " + request.getLocalAddr());
+        System.out.println("request.getLocalPort() = " + request.getLocalPort());
+        System.out.println("--- 기타 조회 end ---");
+        System.out.println();
+    }
 }
