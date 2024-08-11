047c0e5891670717d762756f23a739babbb40e8d
tahee143 <devtaehee@naver.com>
2024-08-11 17:49:18 +0900

    서블릿으로 회원 관리 웹 애플리케이션 만들기

---
+++ 스프링 MVC 1편/servlet/src/test/java/hello/servlet/domain/member/MemberRepositoryTest.java
@@ -0,0 +1,50 @@
+package hello.servlet.domain.member;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+class MemberRepositoryTest {
+
+    MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @AfterEach
+    void afterEach() {
+        memberRepository.clearStore();
+    }
+
+    @Test
+    @DisplayName("회원 저장")
+    void save() {
+        // given
+        Member member = new Member("kim", 20);
+
+        // when
+        Member savedMember = memberRepository.save(member);
+
+        // then
+        Member findMember = memberRepository.findById(savedMember.getId());
+        assertThat(findMember).isEqualTo(savedMember);
+    }
+
+    @Test
+    @DisplayName("모든 회원 조회")
+    void findAll() {
+        // given
+        Member member1 = new Member("kim1", 20);
+        Member member2 = new Member("kim2", 30);
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        // when
+        List<Member> result = memberRepository.findAll();
+
+        // then
+        assertThat(result.size()).isEqualTo(2);
+        assertThat(result).contains(member1, member2);
+    }
+}
