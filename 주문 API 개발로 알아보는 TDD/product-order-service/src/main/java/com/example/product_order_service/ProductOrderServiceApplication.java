1e1561f3cc88543443ac6798ae2054e7f2b53eb7
tahee143 <devtaehee@naver.com>
2024-08-13 11:20:17 +0900

    initial commit

---
+++ 주문 API 개발로 알아보는 TDD/product-order-service/src/main/java/com/example/product_order_service/ProductOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.product_order_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductOrderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductOrderServiceApplication.class, args);
+	}
+
+}
