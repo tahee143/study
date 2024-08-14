823c02e614217b93fb36874e1ed51c9187990bbf
tahee143 <devtaehee@naver.com>
2024-08-14 15:59:20 +0900

    API테스트로 전환

--- 주문 API 개발로 알아보는 TDD/product-order-service/src/test/java/com/example/product_order_service/product/ProductApiTest.java
+++ 주문 API 개발로 알아보는 TDD/product-order-service/src/test/java/com/example/product_order_service/product/ProductApiTest.java
@@ -6,7 +6,6 @@ import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -19,6 +18,20 @@ class ProductApiTest extends ApiTest {
         final var response = ProductSteps.상품등록요청(request); // API 요청
 
         assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+    }
+
+    @Test
+    void 상품조회() {
+        ProductSteps.상품등록요청(ProductSteps.상품등록요청_생성());
+        Long productId = 1L;
+
+        final ExtractableResponse<Response> response = RestAssured.given().log().all()
+                .when()
+                .get("/products/{productId}", productId)
+                .then().log().all()
+                .extract();
+
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
     }
 
 }
