4099c0f3908cb8107aa466107ddc1b6d48bb1eeb
tahee143 <devtaehee@naver.com>
2024-08-14 14:38:16 +0900

    api 테스트로 전환

--- 주문 API 개발로 알아보는 TDD/product-order-service/src/main/java/com/example/product_order_service/product/ProductService.java
+++ 주문 API 개발로 알아보는 TDD/product-order-service/src/main/java/com/example/product_order_service/product/ProductService.java
@@ -1,19 +1,29 @@
 package com.example.product_order_service.product;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Component
+@RestController
+@RequestMapping("/products")
 public class ProductService {
 
     private final ProductPort productPort;
 
-    ProductService(ProductPort productPort) {
+    ProductService(final ProductPort productPort) {
         this.productPort = productPort;
     }
 
-    public void addProduct(final AddProductRequest request) {
+    @PostMapping
+    public ResponseEntity<Void> addProduct(@RequestBody final AddProductRequest request) {
         final Product product = new Product(request.name(), request.price(), request.discountPolicy());
 
         productPort.save(product);
+
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 }
