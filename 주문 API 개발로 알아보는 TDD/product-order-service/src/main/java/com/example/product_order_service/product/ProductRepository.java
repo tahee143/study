ffe2069b5f8755753685b3defde861b6e1342e5b
tahee143 <devtaehee@naver.com>
2024-08-14 15:04:09 +0900

    jpa 적용하기

--- 주문 API 개발로 알아보는 TDD/product-order-service/src/main/java/com/example/product_order_service/product/ProductRepository.java
+++ 주문 API 개발로 알아보는 TDD/product-order-service/src/main/java/com/example/product_order_service/product/ProductRepository.java
@@ -1,18 +1,6 @@
 package com.example.product_order_service.product;
 
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.HashMap;
-import java.util.Map;
-
-@Repository
-public class ProductRepository {
-    private Map<Long, Product> persistence = new HashMap<>();
-    private Long sequence = 0L;
-
-    public void save(Product product) {
-
-        product.assignId(++sequence);
-        persistence.put(product.getId(), product);
-    }
+public interface ProductRepository extends JpaRepository<Product, Long> {
 }
--- 주문 API 개발로 알아보는 TDD/product-order-service/src/main/java/com/example/product_order_service/product/ProductService.java
+++ 주문 API 개발로 알아보는 TDD/product-order-service/src/main/java/com/example/product_order_service/product/ProductService.java
@@ -3,6 +3,7 @@ package com.example.product_order_service.product;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +20,7 @@ public class ProductService {
     }
 
     @PostMapping
+    @Transactional
     public ResponseEntity<Void> addProduct(@RequestBody final AddProductRequest request) {
         final Product product = new Product(request.name(), request.price(), request.discountPolicy());
 
