d51c599893903b5453d5272bba2b24948266cb5f
tahee143 <devtaehee@naver.com>
2024-08-18 02:27:13 +0900

    jpa 적용하기

--- 주문 API 개발로 알아보는 TDD/product-order-service/src/main/java/com/example/product_order_service/product/Order.java
+++ 주문 API 개발로 알아보는 TDD/product-order-service/src/main/java/com/example/product_order_service/product/Order.java
@@ -1,24 +1,27 @@
 package com.example.product_order_service.product;
 
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.util.Assert;
 
+@Entity
+@Table(name = "orders")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
 public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private final Product product;
-    private final int quantity;
+    @OneToOne
+    private Product product;
+    private int quantity;
 
     public Order(final Product product, final int quantity) {
         Assert.notNull(product, "상품은 필수입니다.");
         Assert.isTrue(quantity > 0, "수량은 0보다 커야합니다.");
         this.product = product;
         this.quantity = quantity;
-    }
-
-    public void assignId(Long id) {
-        this.id = id;
-    }
-
-    public Long getId() {
-        return id;
     }
 }
--- 주문 API 개발로 알아보는 TDD/product-order-service/src/main/java/com/example/product_order_service/product/OrderRepository.java
+++ 주문 API 개발로 알아보는 TDD/product-order-service/src/main/java/com/example/product_order_service/product/OrderRepository.java
@@ -1,18 +1,6 @@
 package com.example.product_order_service.product;
 
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.HashMap;
-import java.util.Map;
-
-@Repository
-public class OrderRepository {
-
-    private Map<Long, Order> persistance = new HashMap<>();
-    private Long sequence = 0L;
-
-    public void save(Order order) {
-        order.assignId(++sequence);
-        persistance.put(order.getId(), order);
-    }
+interface OrderRepository extends JpaRepository<Order, Long> {
 }
