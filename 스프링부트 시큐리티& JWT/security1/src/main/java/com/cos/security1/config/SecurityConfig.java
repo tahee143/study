54981871a38b055f717b2d3774358d9055c0d2f3
tahee143 <devtaehee@naver.com>
2024-08-20 05:48:02 +0900

    시큐리티 설정

---
+++ 스프링부트 시큐리티& JWT/security1/src/main/java/com/cos/security1/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.cos.security1.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity // 스프링 시큐리티 필터가 스프링 필터체인에 등록
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/user/**").authenticated() // /user라는 url로 들어오면 인증이 필요하다.
+                        .requestMatchers("/manager/**").hasAnyRole("MANAGER", "ADMIN") // manager으로 들어오는 MANAGER 인증 또는 ADMIN인증이 필요하다는 뜻이다.
+                        .requestMatchers("/admin/**").hasRole("ADMIN") // //admin으로 들어오면 ADMIN권한이 있는 사람만 들어올 수 있음
+                        .anyRequest().permitAll() // 그리고 나머지 url은 전부 권한을 허용해준다.
+                );
+
+        http.formLogin(form -> form
+                .loginPage("/login"));
+        return http.build();
+    }
+
+}
