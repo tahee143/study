b5f0652c5834ab0a0f84684d55f555269a5596e4
tahee143 <devtaehee@naver.com>
2024-08-21 23:38:57 +0900

    시큐리티 권한 처리

--- 스프링부트 시큐리티& JWT/security1/src/main/java/com/cos/security1/config/SecurityConfig.java
+++ 스프링부트 시큐리티& JWT/security1/src/main/java/com/cos/security1/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.cos.security1.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -10,6 +11,7 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity // 스프링 시큐리티 필터가 스프링 필터체인에 등록
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true) // secured 어노테이션 활성화, preAuthorize/postAuthorize 어노테이션 활성화
 public class SecurityConfig {
 
     @Bean
@@ -22,7 +24,7 @@ public class SecurityConfig {
         http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/user/**").authenticated() // /user라는 url로 들어오면 인증이 필요하다.
-                        .requestMatchers("/manager/**").hasAnyRole("MANAGER", "ADMIN") // /manager으로 들어오는 MANAGER 인증 또는 ADMIN인증이 필요하다는 뜻이다.
+                        .requestMatchers("/manager/**").hasAnyRole("ADMIN", "MANAGER") // /manager으로 들어오는 MANAGER 인증 또는 ADMIN인증이 필요하다는 뜻이다.
                         .requestMatchers("/admin/**").hasRole("ADMIN") // //admin으로 들어오면 ADMIN권한이 있는 사람만 들어올 수 있음
                         .anyRequest().permitAll() // 그리고 나머지 url은 전부 권한을 허용해준다.
                 );
--- 스프링부트 시큐리티& JWT/security1/src/main/java/com/cos/security1/controller/IndexController.java
+++ 스프링부트 시큐리티& JWT/security1/src/main/java/com/cos/security1/controller/IndexController.java
@@ -3,6 +3,9 @@ package com.cos.security1.controller;
 import com.cos.security1.repository.UserRepository;
 import com.cos.security1.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -57,6 +60,18 @@ public class IndexController {
         userRepository.save(user);
 
         return "redirect:/loginForm";
+    }
+
+    @Secured("ROLE_ADMIN")
+    @GetMapping("/info")
+    public @ResponseBody String info() {
+        return "개인정보";
+    }
+
+    @PreAuthorize("hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')")
+    @GetMapping("/data")
+    public @ResponseBody String data() {
+        return "데이터정보";
     }
 
 }
