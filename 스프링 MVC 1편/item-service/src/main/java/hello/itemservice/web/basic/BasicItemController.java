254f0782a1714c0f2c952abca93447327ad30512
tahee143 <devtaehee@naver.com>
2024-09-12 17:04:54 +0900

    RedirectAttributes

--- 스프링 MVC 1편/item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ 스프링 MVC 1편/item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -51,7 +52,7 @@ public class BasicItemController {
     }
 
 //    @PostMapping("/add")
-    public String addItemV2(@ModelAttribute("item") Item item, Model model){
+    public String 하addItemV2(@ModelAttribute("item") Item item, Model model){
         itemRepository.save(item);
         model.addAttribute(item);
         return "basic/item";
@@ -72,10 +73,21 @@ public class BasicItemController {
     /**
      * PRG: Post/Redirect/Get
      */
-    @PostMapping("/add")
+//    @PostMapping("/add")
     public String addItemV5(Item item){
         itemRepository.save(item);
         return "redirect:/basic/items/" + item.getId();
+    }
+
+    /**
+     * RedirectAttributes
+     */
+    @PostMapping("/add")
+    public String addItemV6(Item item, RedirectAttributes redirectAttributes) {
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/basic/items/{itemId}";
     }
 
 
