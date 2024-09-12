d11d6b07908747cace8504943346cb5a951cde1b
tahee143 <devtaehee@naver.com>
2024-09-12 16:50:07 +0900

    PRG: Post/Redirect/Get

--- 스프링 MVC 1편/item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ 스프링 MVC 1편/item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -63,11 +63,21 @@ public class BasicItemController {
         return "basic/item";
     }
 
-    @PostMapping("/add")
+//    @PostMapping("/add")
     public String addItemV4(Item item){
         itemRepository.save(item);
         return "basic/item";
     }
+
+    /**
+     * PRG: Post/Redirect/Get
+     */
+    @PostMapping("/add")
+    public String addItemV5(Item item){
+        itemRepository.save(item);
+        return "redirect:/basic/items/" + item.getId();
+    }
+
 
     @GetMapping("/{itemId}/edit")
     public String editForm(@PathVariable Long itemId, Model model) {
